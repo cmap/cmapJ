c88d760ffd58215e8bbd4f3d3739394083a3d33e
Jacob Asiedu <jasiedu@broadinstitute.org>
2017-12-01 13:32:16 -0500

    Fixing adding ignore. Also fix shell scrip

--- runGCTXReaderExampleMacOsX.sh
+++ runGCTXReaderExampleMacOsX.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "Running on 64 bit MacOsX"
+export CLASSPATH="examples:src:lib/jhdf.jar:lib/jhdf5.jar:lib/jhdf5obj.jar:lib/jhdfobj.jar"
 java -Djava.library.path=lib/native/macosx/64 -cp $CLASSPATH ReadGctxExample
